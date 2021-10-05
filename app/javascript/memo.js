76e500ab64a60ace9e8148a84f05cfcc3d33f245
uesei777 <sreiibyoa.1227@i.softbank.jp>
2021-10-05 10:52:55 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,7 +1,8 @@
 function post() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
-    e.preventDefault();  const form = document.getElementById("form");
+    e.preventDefault();
+    const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
